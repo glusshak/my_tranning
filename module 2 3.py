c85071a2e7817a4e197d4b08a9378552ec4bbfac
glusshak <arstupakova@yandex.ru>
2024-08-24 12:28:17 +0300

    Add files via upload

---
+++ module 2 3.py
@@ -0,0 +1,25 @@
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+a = 0
+while (a <= my_list[i]):
+    if(my_list[i] == 0):
+        i += 1
+        continue
+    print(my_list[i])
+    i += 1
+
+
+"""running = True
+while running:
+    if(my_list[i] == 0):
+        i += 1
+        continue
+    elif (a < my_list[i]):
+        print(my_list[i])
+    else:
+        running = False
+    i += 1"""
+
+
+
