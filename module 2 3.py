2609bba94b9f919edff3007cbf380a37412ad936
glusshak <arstupakova@yandex.ru>
2024-09-01 10:24:06 +0300

    Update module 2 3.py

--- module 2 3.py
+++ module 2 3.py
@@ -2,15 +2,7 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
 a = 0
-while (a <= my_list[i]):
-    if(my_list[i] == 0):
-        i += 1
-        continue
-    print(my_list[i])
-    i += 1
-
-
-"""running = True
+running = True
 while running:
     if(my_list[i] == 0):
         i += 1
@@ -19,7 +11,7 @@ while running:
         print(my_list[i])
     else:
         running = False
-    i += 1"""
+    i += 1
 
 
 
